524f6623b669f538899482fc4255e44aaad6a5ca
Pandeyviii <vishalkumarpandey665@gmail.com>
2023-05-22 21:00:46 +0530

    models implementation

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
-const products=[];
+//const products=[];
+const Product=require("../models/product")
 
 exports.getAddProduct=(req, res, next) => {
     res.render('add-product', {
@@ -11,18 +12,30 @@ exports.getAddProduct=(req, res, next) => {
 };
 
 exports.postAddProduct= (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product=new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProduct=(req, res, next) => {
   //const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
+  //const products=Product.fetchAll();
+  // res.render('shop', {
+  //   prods: products,
+  //   pageTitle: 'Shop',
+  //   path: '/',
+  //   hasProducts: products.length > 0,
+  //   activeShop: true,
+  //   productCSS: true
+  // });
+  Product.fetchAll((products)=>{
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+      });
+  })
 }
